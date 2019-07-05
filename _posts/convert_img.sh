0d5e060f6bd37a6f847d7a3ecd49d344480a8e6e
Staf Wagemakers <staf@wagemakers.be>
2019-07-05 20:45:21 +0200

    migrate img tags

--- _posts/convert_img.sh
+++ _posts/convert_img.sh
@@ -1,5 +1,5 @@
 # sed -e 's/\({% *img *.*[^ ]*\) \(\/images\/[^ ]*\)\( .*}\)/\1 \/blog\2\3/g' -i *.markdown
 # sed -e 's/\(<a href="\)\(\/images\)/\1\/blog\2/g' -i *.markdown
 # sed -e 's/\(<img src="\)\(\/images\)/\1\/blog\2/g' -i *.markdown
-sed -e "s/{% *img *\([^ ]*\) \/blog\/\([^ ]*\) \([^ ]*\) \([^ ]*\) \([^ ]*\).*%}/<img src=\"{{ '\/\2'  | relative_url }}\" class=\"\1\" width=\"\3\" height=\"\4\" alt=\5 \/>/g" -i *.markdown 
-sed -e 's/{% *img *\([^ ]*\) \([^ ]*\) \([^ ]*\) \([^ ]*\) \([^ ]*\).*%}/<img src="\2" class=\"\1\" width=\"\3\" height=\"\4\" alt=\"\5\" \/>/g' -i *.markdown 
+sed -e "s/{% img *\([^ ]*\) \/blog\/\([^ ]*\) \([^ ]*\) \([^ ]*\) \([^ ]*\).*%}/<img src=\"{{ '\/\2'  | relative_url }}\" class=\"\1\" width=\"\3\" height=\"\4\" alt=\5 \/>/g" -i *.markdown 
+sed -e 's/{% img *\([^ ]*\) \([^ ]*\) \([^ ]*\) \([^ ]*\) \([^ ]*\).*%}/<img src="\2" class=\"\1\" width=\"\3\" height=\"\4\" alt=\"\5\" \/>/g' -i *.markdown 
